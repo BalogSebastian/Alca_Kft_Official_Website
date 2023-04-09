24efef8e70971bc8e2610520c8d91aa37ec130a5
Balog Sebastian <sebimbalog@gmail.com>
2023-04-09 12:30:57 +0200

    Now the privacy page is responsive.

--- alca_front_end/src/footer_pages_components/Privacy.jsx
+++ alca_front_end/src/footer_pages_components/Privacy.jsx
@@ -3,13 +3,17 @@ import aran from '../assets/aran.mp4';
 
 const Privacy = () =>{
   return (
-    <div className='h-screen w-full relative' style={{ backgroundColor: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-      <div className='text-white object-cover'></div>
-           <h1 className='text-white m-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laboriosam voluptate harum est, minus molestias, omnis quasi
+    <div className='relative object-cover' style={{ backgroundColor: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+      <div className='flex justify-center items-center max-w-[700px] mx-auto w-full p-3 rounded-md text-white'></div>
+           <h1 className='text-white m-20 z-10 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laboriosam voluptate harum est, minus molestias, omnis quasi
+           mollitia similique, eos id quia? Nisi est iste explicabo magni molestias eum eveniet!</h1>
+           <h1 className='text-white m-20 z-10 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laboriosam voluptate harum est, minus molestias, omnis quasi
+           mollitia similique, eos id quia? Nisi est iste explicabo magni molestias eum eveniet!</h1>
+           <h1 className='text-white m-20 z-10 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laboriosam voluptate harum est, minus molestias, omnis quasi
            mollitia similique, eos id quia? Nisi est iste explicabo magni molestias eum eveniet!</h1>
         <li className='text-white my-3'>1. pont</li>
         <li className='text-white justify-center my-3'>2. pont</li>
-        <li className='text-white my-3'>3. pont</li>
+        <li className='text-white mb-20'>3. pont</li>
         </div>
   );
 }
